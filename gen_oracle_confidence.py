e33482d4cc1b639f5d082793f4336a58ac470665
Alexandru Tifrea <tifreaa@marmot.inf.ethz.ch>
2022-06-30 16:56:31 +0200

    small change to gen oracle confidence

--- gen_oracle_confidence.py
+++ gen_oracle_confidence.py
@@ -1,9 +1,11 @@
+import os
 from copy import deepcopy
 import argparse
 import numpy as np
 import torch
 from utils import get_dataset, get_net, get_strategy
 from pprint import pprint
+import pickle
 
 
 if __name__ == "__main__":
@@ -11,7 +13,9 @@ if __name__ == "__main__":
     parser.add_argument('--root', type=str, default=".", help="root")
     parser.add_argument('--ckpt_path', type=str, default="", help="ckpt path")
     parser.add_argument('--seed', type=int, default=1, help="random seed")
-    parser.add_argument('--dataset_name', type=str, default="MNIST", choices=["MNIST", "FashionMNIST", "SVHN", "CIFAR10"], help="dataset")
+    parser.add_argument('--dataset_name', type=str, default="MNIST",
+            choices=["MNIST", "FashionMNIST", "SVHN", "CIFAR10", "CIFAR100",
+            "EuroSAT", "PCAM"], help="dataset")
     args = parser.parse_args()
     pprint(vars(args))
     print()
@@ -33,7 +37,17 @@ if __name__ == "__main__":
 
     print("Generate confidences")
     _, train_data = dataset.get_train_data()
-    # train_preds = strategy.predict(train_data)
-    # train_acc = dataset.cal_labeled_acc(train_preds, np.arange(train_data.shape[0]))
+    train_pred_probs = strategy.predict_prob(train_data)
+    confidences, train_preds = train_pred_probs.max(1)
+    train_acc = dataset.cal_labeled_acc(train_preds, np.arange(train_data.Y.shape[0]))
 
-    # TODO: save in a file for this dataset
+    print("Train acc", train_acc)
+    print(train_preds[:5])
+    print()
+    print(train_pred_probs[:5])
+    print()
+    print(confidences.shape)
+    print(confidences[confidences < 1.0].shape)
+
+    with open(os.path.join(args.root, f'data/{args.dataset_name}/confidences.pkl'), 'wb') as f:
+        pickle.dump(confidences, f)
